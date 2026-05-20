b00cb7091c4e0a6a55467ac6810731f0a62ecf0f
Atiya <atiyaali653@gmail.com>
2026-05-20 17:00:29 +0500

    >

--- backend/src/main/java/com/jhordyess/demouser/config/CorsConfig.java
+++ backend/src/main/java/com/jhordyess/demouser/config/CorsConfig.java
@@ -14,9 +14,13 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
-                        .allowedMethods("*")
-                        .allowedHeaders("*");
+                        .allowedOrigins(
+                                "http://localhost:3000",
+                                "http://127.0.0.1:3000"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
             }
         };
     }
